85242315b3848d2f3261dfe0d83076e24921e63d
Ivan Mutsko <imucko1996@gmail.com>
2023-06-27 19:39:02 +0300

    add mongo

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+//==========================
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://aszxdc95:6VdRhT3T3h0wOvwy@cluster0.aj3lxnx.mongodb.net/db-contacts";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+//==========================
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
