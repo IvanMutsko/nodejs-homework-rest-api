1087119860bb17337ce4ada71264f94a16d2dddd
Ivan Mutsko <imucko1996@gmail.com>
2023-06-27 20:04:17 +0300

    hide pass

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const app = require("./app");
-
-//==========================
 const mongoose = require("mongoose");
-
-const DB_HOST =
-  "mongodb+srv://aszxdc95:6VdRhT3T3h0wOvwy@cluster0.aj3lxnx.mongodb.net/db-contacts";
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -18,8 +15,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-//==========================
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
