d6eed2bd586b84ba56e5e973b7f328df571415fc
Ivan Mutsko <imucko1996@gmail.com>
2023-06-28 21:34:38 +0300

    add mongo

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewars");
-const schemas = require("../../schemas/contacts");
+const { isValidId, validateBody } = require("../../middlewars");
+const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:contactId", ctrl.getContactById);
+router.get("/:contactId", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateContact);
+router.put(
+  "/:contactId",
+  validateBody(schemas.addSchema),
+  isValidId,
+  ctrl.updateContact
+);
 
-router.delete("/:contactId", ctrl.removeContact);
+router.patch(
+  "/:contactId/favorite",
+  validateBody(schemas.updateFavoriteSchema),
+  isValidId,
+  ctrl.updateFavorite
+);
+
+router.delete("/:contactId", isValidId, ctrl.removeContact);
 
 module.exports = router;
