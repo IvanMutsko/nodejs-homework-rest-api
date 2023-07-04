02e4e6c5f191c1f6eaab680aee7d093e6e6442c8
Ivan Mutsko <imucko1996@gmail.com>
2023-07-04 22:38:42 +0300

    add logout, authenticate

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,19 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find({}, "-createdAt -updatedAt");
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, favorite } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "email subscription");
   res.json(result);
 };
 
@@ -16,7 +28,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
